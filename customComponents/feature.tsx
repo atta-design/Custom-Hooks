1c28f91fd61baf4f438747b8f3778fcc41ec8031
atta <attasamadi3@gmail.com>
2025-06-03 16:30:54 +0330

    add lorem in feature file

--- customComponents/feature.tsx
+++ customComponents/feature.tsx
@@ -4,6 +4,11 @@ export default function feature() {
     return (
         <div>
             <h1>Feature</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Aliquid ducimus quasi impedit maxime quae, voluptas recusandae
+                  consequuntur eius consequatur?
+                 Reiciendis neque blanditiis optio excepturi enim iure praesentium 
+                 velit harum! Soluta?</p>
         </div>
     )
 }
